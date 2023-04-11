bd6b0ee3e88994bd33c9451dc0a2500388c375bb
Alvin <alvinmartindwrk@gmail.com>
2023-04-11 21:15:02 +0700

    fix age validation

--- models/user.go
+++ models/user.go
@@ -11,7 +11,7 @@ type User struct {
 	Username     string        `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~username is required"`
 	Email        string        `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~email is required,email~invalid email format"`
 	Password     string        `gorm:"not null" json:"password" form:"password" valid:"required~password is required,minstringlength(6)~password must have a minimum length of 6 characters"`
-	Age          int           `gorm:"not null" json:"age" form:"age" valid:"required~age is required,min=8~user must be at least 8 years old"`
+	Age          int           `gorm:"not null" json:"age" form:"age" valid:"required~age is required,range(8|99)~user must be at least 8 years old"`
 	Photos       []Photo       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"photos"`
 	Comments     []Comment     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"comments"`
 	SocialMedias []SocialMedia `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"social_medias"`
