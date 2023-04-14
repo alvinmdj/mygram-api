4b0c17ab26abd9c6b63fcf1ffbc2a7371c9edf36
Alvin <alvinmartindwrk@gmail.com>
2023-04-14 13:43:12 +0700

    refactor cloudinary helper and add destroy function

--- helpers/cloudinary.go
+++ helpers/cloudinary.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"log"
 	"mime/multipart"
+	"net/url"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
@@ -29,16 +33,50 @@ func InitCloudinary() {
 	log.Println("cloudinary connected successfully")
 }
 
+func publicIdPath(fileName string) string {
+	return "photos/" + fileName
+}
+
+func getFileNameFromUrl(fileUrlString string) string {
+	fileUrl, err := url.Parse(fileUrlString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// return last path from url
+	// example url: https://res.cloudinary.com/xxx/image/upload/yyy/photos/file-name.png"
+	// return value: file-name.png
+	fileNameWithExt := path.Base(fileUrl.Path)
+
+	// remove extension to get the file-name
+	// from 'file-name.png' to 'file-name'
+	return strings.TrimSuffix(fileNameWithExt, filepath.Ext(fileNameWithExt))
+}
+
 func UploadToCloudinary(file multipart.File) (string, error) {
 	ctx := context.Background()
-	publicId := uuid.New()
+	fileName := uuid.New()
 
 	resp, err := cld.Upload.Upload(ctx, file, uploader.UploadParams{
-		PublicID: "photos/" + publicId.String(), // folder-name/file-name
+		PublicID: publicIdPath(fileName.String()), // folder-name/file-name
 	})
 	if err != nil {
 		log.Printf("error uploading file to cloudinary: %v", err.Error())
 	}
 
 	return resp.SecureURL, err
+}
+
+func DestroyFromCloudinary(fileUrlString string) (err error) {
+	ctx := context.Background()
+
+	fileName := getFileNameFromUrl(fileUrlString)
+
+	_, err = cld.Upload.Destroy(ctx, uploader.DestroyParams{
+		PublicID: publicIdPath(fileName),
+	})
+	if err != nil {
+		log.Printf("error trying to delete file '%v' from cloudinary: %v", fileUrlString, err.Error())
+	}
+	return
 }
