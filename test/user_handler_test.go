f1217c6857946a03bcea079f97f1ef0816634437
Alvin <alvinmartindwrk@gmail.com>
2023-09-19 21:11:08 +0700

    test user register failed

--- test/user_handler_test.go
+++ test/user_handler_test.go
@@ -54,3 +54,36 @@ func TestRegisterSuccess(t *testing.T) {
 	assert.Equal(t, "alvinmdj@mygram.com", responseBody.Email)
 	assert.Equal(t, 21, responseBody.Age)
 }
+
+func TestRegisterFailed(t *testing.T) {
+	db := SetupTestDB()
+	TruncateUsersTable(db)
+	gin.SetMode(gin.ReleaseMode)
+	router := routers.StartApp(db)
+
+	// Empty input
+	input := models.UserRegisterInput{}
+
+	// Marshal the struct into a JSON string
+	jsonBody, _ := json.Marshal(input)
+
+	// Create an io.Reader from the JSON string
+	requestBody := strings.NewReader(string(jsonBody))
+	request, _ := http.NewRequest(http.MethodPost, "/api/v1/users/register", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 400, response.StatusCode)
+
+	// Read the response body & unmarshal the response into UserRegisterOutput struct
+	body, _ := io.ReadAll(response.Body)
+	var responseBody models.ErrorResponse
+	json.Unmarshal(body, &responseBody)
+
+	assert.NotNil(t, responseBody.Error)
+	assert.Equal(t, "BAD REQUEST", responseBody.Error)
+	assert.NotNil(t, responseBody.Message)
+}
