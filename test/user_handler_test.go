a861366efc0a956126932c670bff7d19d824c5ff
Alvin <alvinmartindwrk@gmail.com>
2023-09-20 14:50:07 +0700

    user login handler test

--- test/user_handler_test.go
+++ test/user_handler_test.go
@@ -9,12 +9,27 @@ import (
 	"testing"
 
 	"github.com/alvinmdj/mygram-api/models"
+	"github.com/alvinmdj/mygram-api/repositories"
 	"github.com/alvinmdj/mygram-api/routers"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
+// function to create a user before login test
+func registerTestUser(db *gorm.DB) {
+	userData := models.User{
+		Username: "alvinmdj",
+		Email:    "alvinmdj@mygram.com",
+		Password: "password",
+		Age:      20,
+	}
+	userRepo := repositories.NewUserRepo(db)
+	userRepo.Save(userData)
+}
+
 func TestRegisterSuccess(t *testing.T) {
+	// Setup
 	db := SetupTestDB()
 	TruncateUsersTable(db)
 	gin.SetMode(gin.ReleaseMode)
@@ -56,6 +71,7 @@ func TestRegisterSuccess(t *testing.T) {
 }
 
 func TestRegisterFailed(t *testing.T) {
+	// Setup
 	db := SetupTestDB()
 	TruncateUsersTable(db)
 	gin.SetMode(gin.ReleaseMode)
@@ -78,12 +94,90 @@ func TestRegisterFailed(t *testing.T) {
 	response := recorder.Result()
 	assert.Equal(t, 400, response.StatusCode)
 
-	// Read the response body & unmarshal the response into UserRegisterOutput struct
+	// Read the response body & unmarshal the response into ErrorResponse struct
 	body, _ := io.ReadAll(response.Body)
 	var responseBody models.ErrorResponse
 	json.Unmarshal(body, &responseBody)
 
 	assert.NotNil(t, responseBody.Error)
 	assert.Equal(t, "BAD REQUEST", responseBody.Error)
+	assert.NotNil(t, responseBody.Message)
+}
+
+func TestLoginSuccess(t *testing.T) {
+	// Setup
+	db := SetupTestDB()
+	TruncateUsersTable(db)
+	gin.SetMode(gin.ReleaseMode)
+	router := routers.StartApp(db)
+
+	// Register a new test user
+	registerTestUser(db)
+
+	// Create an instance of UserLoginInput struct
+	input := models.UserLoginInput{
+		Email:    "alvinmdj@mygram.com",
+		Password: "password",
+	}
+
+	// Marshal the struct into a JSON string
+	jsonBody, _ := json.Marshal(input)
+
+	// Create an io.Reader from the JSON string
+	requestBody := strings.NewReader(string(jsonBody))
+	request, _ := http.NewRequest(http.MethodPost, "/api/v1/users/login", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 201, response.StatusCode)
+
+	// Read the response body & unmarshal the response into UserLoginOutput struct
+	body, _ := io.ReadAll(response.Body)
+	var responseBody models.UserLoginOutput
+	json.Unmarshal(body, &responseBody)
+
+	assert.NotNil(t, responseBody.Token)
+}
+
+func TestLoginFailed(t *testing.T) {
+	// Setup
+	db := SetupTestDB()
+	TruncateUsersTable(db)
+	gin.SetMode(gin.ReleaseMode)
+	router := routers.StartApp(db)
+
+	// Register a new test user
+	registerTestUser(db)
+
+	// Create an instance of UserLoginInput struct with wrong credentials
+	input := models.UserLoginInput{
+		Email:    "wrong@test.com",
+		Password: "hahahaha",
+	}
+
+	// Marshal the struct into a JSON string
+	jsonBody, _ := json.Marshal(input)
+
+	// Create an io.Reader from the JSON string
+	requestBody := strings.NewReader(string(jsonBody))
+	request, _ := http.NewRequest(http.MethodPost, "/api/v1/users/login", requestBody)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	assert.Equal(t, 401, response.StatusCode)
+
+	// Read the response body & unmarshal the response into ErrorResponse struct
+	body, _ := io.ReadAll(response.Body)
+	var responseBody models.ErrorResponse
+	json.Unmarshal(body, &responseBody)
+
+	assert.NotNil(t, responseBody.Error)
+	assert.Equal(t, "UNAUTHORIZED", responseBody.Error)
 	assert.NotNil(t, responseBody.Message)
 }
